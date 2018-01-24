41c97a495bd524454eec63af6437dfb3e5cedcb2
Pavan Kumar <venkata.sankarapu@gmail.com>
2018-01-23 19:54:49 -0600

    Methods

--- AttResource.py
+++ AttResource.py
@@ -25,23 +25,11 @@ def get_all_open_issues():
         repo_info['repository_id'] = item['id']
         iss_list = []
         for issue in repo_issues:
-            issue_info = {}
-            issue_info['issue_id'] = issue['id']
-            issue_info['issue_number'] = issue['number']
-            issue_info['issue_title'] = issue['title']
-            issue_info['issue_description'] = issue['body']
-            issue_info['issue_created_at'] = issue['created_at']
-            issue_info['issue_created_by_user'] = issue['user']['id']
-            issue_info['issue_status'] = issue['state']
+            issue_info = method_name(issue)
             issue_comments = requests.get(issue['comments_url'], headers={"Authorization": "Basic ZGV2aWthLjUyMEBnbWFpbC5jb206ZGV2aWthMTIz"}).json()
             iss_comments = []
             for issue_comment in issue_comments:
-                comment = {}
-                comment['comment_id'] = issue_comment['id']
-                comment['comment_created_at'] = issue_comment['created_at']
-                comment['comment_body'] = issue_comment['body']
-                comment['comment_user_id'] = issue_comment['user']['id']
-                iss_comments.append(comment)
+                method_getissues(iss_comments, issue_comment)
             issue_info['comments'] = iss_comments
             iss_list.append(issue_info)
         repo_info['issues'] = iss_list
@@ -50,7 +38,30 @@ def get_all_open_issues():
     (json.dumps(response, indent=2))
 
     return Response(json.dumps(response,indent=2),mimetype='application/json')
- #Response(xml, mimetype='text/xml')
+
+
+def method_getissues(iss_comments, issue_comment):
+    comment = {}
+    comment['comment_id'] = issue_comment['id']
+    comment['comment_created_at'] = issue_comment['created_at']
+    comment['comment_body'] = issue_comment['body']
+    comment['comment_user_id'] = issue_comment['user']['id']
+    iss_comments.append(comment)
+
+
+def method_name(issue):
+    issue_info = {}
+    issue_info['issue_id'] = issue['id']
+    issue_info['issue_number'] = issue['number']
+    issue_info['issue_title'] = issue['title']
+    issue_info['issue_description'] = issue['body']
+    issue_info['issue_created_at'] = issue['created_at']
+    issue_info['issue_created_by_user'] = issue['user']['id']
+    issue_info['issue_status'] = issue['state']
+    return issue_info
+
+
+#Response(xml, mimetype='text/xml')
 #get_all_open_issues()
 
 if __name__ == '__main__':
